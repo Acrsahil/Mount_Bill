a2d8e71cc4857e41b42275d0194a57fb803c3165
Sabina Meche <sabinameche22@gamail.com>
2025-12-01 16:32:00 +0545

    feat:made signup page

--- mount_django/billingsystem/forms.py
+++ mount_django/billingsystem/forms.py
@@ -11,6 +11,7 @@ class UserRegistrationForm(UserCreationForm):
     company_name=forms.CharField(max_length=1500,widget=forms.TextInput(),required=True)
     email=forms.EmailField(widget=forms.EmailInput(),required=True)
     phone_country = forms.ModelChoiceField(queryset=CountryCode.objects.all(), required=True) 
+    phone_number = forms.CharField(max_length=10,required=True)
     class Meta:
         model=UserRegistration
-        fields=['username','email','phone_number','password1','password2','company_name']
+        fields=['username','email','company_name','phone_number','phone_country','password1','password2']
