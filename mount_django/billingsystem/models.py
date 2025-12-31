fbc26075ca845791dd6dd5d984d5bdc365f33871
Sabina Meche <sabinameche22@gamail.com>
2025-12-31 17:09:44 +0545

    model changed search category fix

--- mount_django/billingsystem/models.py
+++ mount_django/billingsystem/models.py
@@ -7,15 +7,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 
-class ItemActivity(models.Model):
-    type = models.CharField(max_length=150)
-    date = models.DateField(auto_now=True)
-    change = models.CharField(max_length=50)
-    quantity = models.IntegerField()
-    remarks = models.CharField(max_length=200,blank=True)
 
-    def __str__(self):
-        return self.type
 
 class User(AbstractUser):
     phone = models.CharField(max_length=15, blank=True)
@@ -105,7 +97,7 @@ class Product(models.Model):
     company = models.ForeignKey(
         Company, on_delete=models.CASCADE, related_name="products"
     )
-    item_acitivity = models.ForeignKey(ItemActivity,on_delete=models.SET_NULL,null=True,related_name="activities")
+    
     uid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     cost_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
@@ -131,7 +123,7 @@ class OrderList(models.Model):
     company = models.ForeignKey(
         Company, on_delete=models.CASCADE, related_name="orders"
     )
-    item_acitivity = models.ForeignKey(ItemActivity,on_delete=models.SET_NULL,null=True,related_name="orderactivities")
+    
     order_date = models.DateTimeField(default=timezone.now)
     customer = models.ForeignKey(
         Customer,
@@ -278,3 +270,15 @@ class RemainingAmount(models.Model):
 
     def __str__(self):
         return f"remaining amount:{self.remaining_amount}"
+
+
+class ItemActivity(models.Model):
+    order = models.ForeignKey(OrderList,on_delete=models.SET_NULL,null=True,related_name="orderactivities")
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True,related_name="activities")
+    date = models.DateField(auto_now=True)
+    change = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    remarks = models.CharField(max_length=200,blank=True)
+
+    def __str__(self):
+        return self.type
