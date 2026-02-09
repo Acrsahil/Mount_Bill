6eb8995b8a9ac1a3ab85b5e617c4248302406f90
sandip tamang <tamangsandip609@gmail.com>
2026-02-09 14:38:48 +0545

    chore: initial tailwind v4 and environment setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <>
-      <h1 class="m-2 flex items-center bg-blue-500 p-4">Hello World!</h1>
+      <h1 class="m-2 flex items-center p-4 text-accent">Hello World!</h1>
     </>
   );
 }
